d4391a9f0c16d1a095348d79ac83e1731a8af924
ivan52945 <ivan52945@gmail.com>
2022-07-02 17:34:47 +0300

    feat: add task module

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    n_abs = (n < 0) ? -n : n;
+    let reverse_number = 0;
+    let string_number = n_abs + '';
+    for (let i = 0; i < string_number.length; i++) {
+        reverse_number += +(string_number[i]) * (10 ** i);
+    }
+    return reverse_number;
 }
+
+
